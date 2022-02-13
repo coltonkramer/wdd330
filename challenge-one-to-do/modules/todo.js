432e442b2355ba8601617fbe59868f9f35557484
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-02-13 00:25:46 -0500

    mostly working to do list

--- challenge-one-to-do/modules/todo.js
+++ challenge-one-to-do/modules/todo.js
@@ -1,62 +1,62 @@
 const listContainer = document.getElementById("list-container");
-const rowContainer = document.createElement("div");
-window.onload = loadTasks();
 let list = [];
-
-function getDate() {
-  return Date.now();
-}
+window.onload = getFromLocalStorage();
 
 document.querySelector(".add-button").addEventListener("click", () => {
-  let task = document.getElementById("form-input").value;
-  const item = {
-    id: getDate(),
-    content: task,
-    completed: false,
-  };
-  list.push(item);
-  console.log(list)
-  let updatedList = JSON.stringify(list);
-  localStorage.setItem("todo-item", updatedList);
-  document.getElementById("form-input").value = "";
-  loadTasks();
+    let task = document.getElementById("form-input").value;
+    if(task === ""){
+        alert('Please add something to the list')
+    } else {
+        const item = {
+            id: Date.now(),
+            content: task,
+            completed: false
+        };
+        
+        list.push(item);
+        addToLocalStorage(list);
+        document.getElementById("form-input").value = "";
+    }
 });
 
-function loadTasks() {
-  let tasks = window.localStorage.getItem("todo-item");
-  let data = JSON.parse(tasks);
-
-  data.forEach(i => {
-      
-      rowContainer.innerHTML = `
-      <input class="check-box" id="${i.id}" type='checkbox' ${
-          i.completed ? "checked" : ""
-        }>
-        <label class="list-element" for="${i.id}">${i.content}</label>
-        <button class="delete-button" id="delete${
-            i.id
-        }" onclick="return this.parentNode.remove();">X</button>
-        `;
-        listContainer.append(rowContainer);
-        
-        console.log(data);
-    });
+function addToLocalStorage(list){
+    let updatedList = JSON.stringify(list);
+    localStorage.setItem("todo-item", updatedList);
+    renderList(list);
+}
+function getFromLocalStorage(){
+    let tasks = window.localStorage.getItem("todo-item");
+    if (tasks) {
+        let data = JSON.parse(tasks);
+        renderList(data);
+    }
 }
 
-// // import { newRow } from "./utilities.js";
-// import { storage } from "./ls.js";
-// // grabs the large container already in the DOM
-// let listContainer = document.getElementById("list-container");
+function renderList(list) {
+    // if (tasks === null) {
+    //     console.log('%c Oh my heavens! ', 'background: #222; color: #bada55')
+    // } else {
+        listContainer.innerHTML = ``; 
+        list.forEach(i => {
+    const rowContainer = document.createElement("div");
+    rowContainer.setAttribute('data-key', i.id); 
+    rowContainer.setAttribute('class', 'item');
+    rowContainer.innerHTML = `
+      <input class="check-box" id="${i.id}" type='checkbox' ${i.completed ? 'checked' : null}>
+        <label class="list-element" for="${i.id}">${i.content}</label>
+        <button class="delete-button">X</button>
+        `;
+      listContainer.append(rowContainer)
 
-//     storage.forEach((i) => {
-//       let id = i["id"];
-//       let content = i["content"];
-//       let completed = i["completed"];
+    })
+//   }
+}
+function checked(id){
+    list.forEach(i => {
+        if (i.id == id){
+            i.completed = !i.completed
+        }
+    });
+    addToLocalStorage(list);
 
-//       if (completed) {
-//         document.getElementById(id).checked = true;
-//       }
-
-//     });
-
-//     //adding and removing things to the array
+}
