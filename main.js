68dbad8df11f6f9225fa081fcdc91f2e9e7048c2
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-02-13 00:32:58 -0500

    added the links to everything to the homepage

--- main.js
+++ main.js
@@ -3,6 +3,26 @@ const links = [
     label: "Week1 notes",
     url: "week1/index.html",
   },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html",
+  },
+  {
+    label: "Week3 notes",
+    url: "week3/index.html",
+  },
+  {
+    label: "Week4 notes",
+    url: "week4/index.html",
+  },
+  {
+    label: "Week5 notes",
+    url: "week5/index.html",
+  },
+  {
+    label: "Todo list",
+    url: "challenge-one-to-do/index.html",
+  },
 ];
 const list = document.querySelector(".assignment-list");
 links.forEach((i) => {
